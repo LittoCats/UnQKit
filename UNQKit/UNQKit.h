139270eb5bb2a41bb4f143471c65f2de48f988b9
程巍巍 <littocats@gmail.com>
2015-08-03 10:58:41 +0800

    Initial Commit

---
+++ UNQKit/UNQKit.h
@@ -0,0 +1,19 @@
+//
+//  UNQKit.h
+//  UNQKit
+//
+//  Created by 程巍巍 on 8/3/15.
+//  Copyright (c) 2015 Littocats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UNQKit.
+FOUNDATION_EXPORT double UNQKitVersionNumber;
+
+//! Project version string for UNQKit.
+FOUNDATION_EXPORT const unsigned char UNQKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UNQKit/PublicHeader.h>
+
+
